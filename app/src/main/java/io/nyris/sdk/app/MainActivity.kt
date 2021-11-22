a13f2f1692604e3dd65dd3970a522ff5fba44483
Mavlianov <ruslan@letmecode.org>
2021-11-22 12:11:54 +0300

    feat: add view binding

--- app/src/main/java/io/nyris/sdk/app/MainActivity.kt
+++ app/src/main/java/io/nyris/sdk/app/MainActivity.kt
@@ -3,35 +3,39 @@ package io.nyris.sdk.app
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.nyris.sdk.app.databinding.ActivityMainBinding
 import io.nyris.sdk.disposable
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val demoApp = application as DemoApp
         val nyris = demoApp.nyris
         lifecycle.addObserver(nyris)
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val inputStream = assets.open("test_image.jpg")
         val byteArray = ByteArray(inputStream.available())
         inputStream.read(byteArray)
         inputStream.close()
 
-        tvResults.text = "Matching ..."
-        nyris.
-            imageMatching()
+        binding.tvResults.text = "Matching ..."
+        nyris
+            .imageMatching()
             .recommendations()
             .match(byteArray)
             .subscribe({
-                tvResults.text = "Matched offers : ${it.offers.size} \n Predicted categories : ${it.predictedCategories}"
+                binding.tvResults.text =
+                    "Matched offers : ${it.offers.size} \n " +
+                            "Predicted categories : ${it.predictedCategories}"
             },{
-                tvResults.text = it.message
+                binding.tvResults.text = it.message
             }).disposable()
     }
 }
